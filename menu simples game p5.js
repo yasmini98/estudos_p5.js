67e736a9409c6af4ce307580d5e8c1f5f07ffd89
Yasmini Oliveira <76539774+yasmini98@users.noreply.github.com>
2021-08-15 01:41:52 -0300

    Update menu simples game p5.js
    Código de um menu simples de jogo com 3 opções de escolhas:  jogar, instruções e créditos. Nele as opções tem um número designado no console podendo usá-los como botões de ação.
     usando as setas do teclado para cima e para baixo voce pode navegar nas opções, apertando o botão "Enter" do teclado você escolhe uma opção e apertando "Esc" você volta para a tela inicial.

--- menu simples game p5.js
+++ menu simples game p5.js
@@ -1,14 +1,25 @@
 var posicaoX = 45;
 var escalaY = 35;
 var posicaoY = 103;
-var opção = 1
+var opção = 1;
+var tela = 0;
 
 function setup() {
 createCanvas(400, 400);
 }
 function draw() {
   background('#FD0076');
-  
+  if(tela==0)
+  menu();
+  if(tela==1)
+    fase1();
+  if(tela==2)
+    instruções();
+  if(tela==3)
+    créditos();
+}
+
+function menu(){
   rect(posicaoX, posicaoY, 150, escalaY);
   
   textSize(50);
@@ -19,11 +30,38 @@ function draw() {
   text('créditos', 50, 330);
 }
 
+function fase1(){
+  textSize(32);
+  text('Fase 1', 90, 130);
+}
+
+function instruções(){
+  textSize(32);
+  text('Instruções', 90, 130);
+}
+
+function créditos(){
+  textSize(32);
+  text('Créditos', 90, 130);
+}
+
 function keyPressed(){
-  if(key=='ArrowUp' && posicaoY>130)
+  if(key=='ArrowUp' && posicaoY>130){
+    
     posicaoY = posicaoY - 100;
-  if(key=='ArrowDown'&& posicaoY<300)
-    posicaoY = posicaoY + 100;
-  opção = opção + 1
+    opção = opção-1
   console.log(opção)
+  }
+  
+  if(key=='ArrowDown'&& posicaoY<300){
+    posicaoY = posicaoY + 100;
+    opção = opção+1
+  console.log(opção)
+  }
+  if(key=='Enter'){
+    tela = opção
+  }
+  if(key=='Escape'){
+    tela = 0
+  }
 }
